15e7e3eed270ddb41d9b03005d44fe74b2d6752b
Phill Sparks <me@phills.me.uk>
2019-12-17 09:06:10 +0000

    Format tests to use reverse pipe <|

--- tests/DifficultyTest.elm
+++ tests/DifficultyTest.elm
@@ -13,52 +13,49 @@ suite =
         [ describe "fromInt"
             [ fuzz (intRange 1 10)
                 "Difficulty can be 1..10"
-                (\n ->
+              <|
+                \n ->
                     Difficulty.fromInt n
                         |> Difficulty.toInt
                         |> Expect.equal n
-                )
-            , test "Difficulty cannot be zero"
-                (\() ->
+            , test "Difficulty cannot be zero" <|
+                \_ ->
                     Difficulty.fromInt 0
                         |> Expect.equal Difficulty.min
-                )
             , fuzz (intRange minInt -1)
                 "Difficulty cannot be negative"
-                (\n ->
+              <|
+                \n ->
                     Difficulty.fromInt n
                         |> Expect.equal Difficulty.min
-                )
             , fuzz (intRange 11 maxInt)
                 "Difficulty cannot be greater than 10"
-                (\n ->
+              <|
+                \n ->
                     Difficulty.fromInt n
                         |> Expect.equal Difficulty.max
-                )
             ]
         , describe "toString"
             [ fuzz (intRange 1 10)
                 "Returns a String of the given difficulty"
-                (\n ->
+              <|
+                \n ->
                     Difficulty.fromInt n
                         |> Difficulty.toString
                         |> Expect.equal (String.fromInt n)
-                )
             ]
         , describe "min"
-            [ test "is 1"
-                (\() ->
+            [ test "is 1" <|
+                \_ ->
                     Difficulty.min
                         |> Difficulty.toInt
                         |> Expect.equal 1
-                )
             ]
         , describe "max"
-            [ test "is 10"
-                (\() ->
+            [ test "is 10" <|
+                \_ ->
                     Difficulty.max
                         |> Difficulty.toInt
                         |> Expect.equal 10
-                )
             ]
         ]
--- tests/ZipListTest.elm
+++ tests/ZipListTest.elm
@@ -21,68 +21,60 @@ suite : Test
 suite =
     describe "ZipList"
         [ describe "singleton"
-            [ test "is a list of the given item"
-                (\() ->
+            [ test "is a list of the given item" <|
+                \_ ->
                     ZipList.singleton 1
                         |> ZipList.toList
                         |> Expect.equal [ 1 ]
-                )
             ]
         , describe "fromLists"
-            [ test "is a list of the given items"
-                (\() ->
+            [ test "is a list of the given items" <|
+                \_ ->
                     ZipList.fromLists [ 1, 2 ] 3 [ 4, 5, 6 ]
                         |> ZipList.toList
                         |> Expect.equal [ 1, 2, 3, 4, 5, 6 ]
-                )
             ]
         , describe "before"
-            [ test "is a list of the items before current"
-                (\() ->
+            [ test "is a list of the items before current" <|
+                \_ ->
                     ZipList.fromLists [ 1, 2 ] 3 [ 4, 5, 6 ]
                         |> ZipList.before
                         |> Expect.equal [ 1, 2 ]
-                )
             ]
         , describe "selected"
-            [ test "is the current item"
-                (\() ->
+            [ test "is the current item" <|
+                \_ ->
                     ZipList.fromLists [ 1, 2 ] 3 [ 4, 5, 6 ]
                         |> ZipList.selected
                         |> Expect.equal 3
-                )
             ]
         , describe "after"
-            [ test "is a list of the items after current"
-                (\() ->
+            [ test "is a list of the items after current" <|
+                \_ ->
                     ZipList.fromLists [ 1, 2 ] 3 [ 4, 5, 6 ]
                         |> ZipList.after
                         |> Expect.equal [ 4, 5, 6 ]
-                )
             ]
         , describe "toList"
-            [ test "is a list of all items"
-                (\() ->
+            [ test "is a list of all items" <|
+                \_ ->
                     ZipList.fromLists [ 1, 2 ] 3 [ 4, 5, 6 ]
                         |> ZipList.toList
                         |> Expect.equal [ 1, 2, 3, 4, 5, 6 ]
-                )
             ]
         , describe "map"
-            [ test "transforms each element of the list"
-                (\() ->
+            [ test "transforms each element of the list" <|
+                \_ ->
                     ZipList.fromLists [ 1, 2 ] 3 [ 4, 5, 6 ]
                         |> ZipList.map (\n -> n * 2)
                         |> ZipList.toList
                         |> Expect.equal [ 2, 4, 6, 8, 10, 12 ]
-                )
-            , test "can change items to a new item type"
-                (\() ->
+            , test "can change items to a new item type" <|
+                \_ ->
                     ZipList.fromLists [ 1, 2 ] 3 [ 4, 5, 6 ]
                         |> ZipList.map String.fromInt
                         |> ZipList.toList
                         |> Expect.equal [ "1", "2", "3", "4", "5", "6" ]
-                )
             ]
         , describe "update"
             [ test "replaces the current item in the list" <|
@@ -92,70 +84,61 @@ suite =
                         |> expectEqualZipLists [ 1, 2 ] 9 [ 4, 5, 6 ]
             ]
         , describe "prepend"
-            [ test "adds new items before the list"
-                (\() ->
+            [ test "adds new items before the list" <|
+                \_ ->
                     ZipList.fromLists [ 1, 2 ] 3 [ 4, 5, 6 ]
                         |> ZipList.prepend [ 7, 8 ]
                         |> ZipList.before
                         |> Expect.equal [ 7, 8, 1, 2 ]
-                )
             ]
         , describe "append"
-            [ test "adds new items after the list"
-                (\() ->
+            [ test "adds new items after the list" <|
+                \_ ->
                     ZipList.fromLists [ 1, 2 ] 3 [ 4, 5, 6 ]
                         |> ZipList.append [ 7, 8 ]
                         |> ZipList.after
                         |> Expect.equal [ 4, 5, 6, 7, 8 ]
-                )
             ]
         , describe "select"
-            [ test "does not change if no item passes the check"
-                (\() ->
+            [ test "does not change if no item passes the check" <|
+                \_ ->
                     ZipList.fromLists [ 1, 2 ] 3 [ 4, 5, 6 ]
                         |> ZipList.select (always False)
                         |> ZipList.selected
                         |> Expect.equal 3
-                )
-            , test "selects the first item to pass the check"
-                (\() ->
+            , test "selects the first item to pass the check" <|
+                \_ ->
                     ZipList.fromLists [ 1, 2 ] 3 [ 4, 5, 6 ]
                         |> ZipList.select isEven
                         |> Expect.equal (ZipList.fromLists [ 1 ] 2 [ 3, 4, 5, 6 ])
-                )
             ]
         , describe "rewind"
-            [ test "with a singleton, is the same list"
-                (\() ->
+            [ test "with a singleton, is the same list" <|
+                \_ ->
                     ZipList.singleton 3
                         |> ZipList.rewind
                         |> Expect.equal (ZipList.singleton 3)
-                )
-            , test "selects the first item"
-                (\() ->
+            , test "selects the first item" <|
+                \_ ->
                     ZipList.fromLists [ 1, 2 ] 3 [ 4, 5, 6 ]
                         |> ZipList.rewind
                         |> Expect.equal (ZipList.fromLists [] 1 [ 2, 3, 4, 5, 6 ])
-                )
             ]
         , describe "next"
-            [ test "with a singleton, is the same list"
-                (\() ->
+            [ test "with a singleton, is the same list" <|
+                \_ ->
                     ZipList.singleton 3
                         |> ZipList.next
                         |> Expect.equal (ZipList.singleton 3)
-                )
-            , test "selects the next item"
-                (\() ->
+            , test "selects the next item" <|
+                \_ ->
                     ZipList.fromLists [ 1, 2 ] 3 [ 4, 5, 6 ]
                         |> ZipList.next
                         |> Expect.equal (ZipList.fromLists [ 1, 2, 3 ] 4 [ 5, 6 ])
-                )
-            , test "at the end of a list, selects the first item"
-                (\() ->
+            , test "at the end of a list, selects the first item" <|
+                \_ ->
                     ZipList.fromLists [ 1, 2, 3, 4, 5 ] 6 []
                         |> ZipList.next
                         |> Expect.equal (ZipList.fromLists [] 1 [ 2, 3, 4, 5, 6 ])
-                )
             ]
         ]
