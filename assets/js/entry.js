126fbce3d3a49238d83b9ff857858bed9ebfda01
Phill Sparks <me@phills.me.uk>
2019-12-21 09:49:21 +0000

    Format entry.js

--- assets/js/entry.js
+++ assets/js/entry.js
@@ -1,7 +1,7 @@
 import "../sass/style.scss";
 import { Elm } from "../../src/Main.elm";
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const mountPoint = document.querySelector("main");
     Elm.Main.init({
         node: mountPoint,
